5d9d84cfec1dbc8baba4a45b663d21ab90357701
BoboLord_ <saisreedeep@outlook.com>
2018-02-04 20:41:15 +0530

    Updated server list

--- server/services/dataService.js
+++ server/services/dataService.js
@@ -1,11 +1,18 @@
 var clusters = [{"clusterID":0, "clusterName":"Development"},
-                {"clusterID":1, "clusterName":"Testing"}
+                {"clusterID":1, "clusterName":"Testing"},
+                {"clusterID":2, "clusterName":"Miscelleneous"},
                 ];
-const servers = [{"id":0,"clusterID":0,"name":"USERApp","host":"http://10.206.0.80","port":8090},
-{"id":1,"clusterID":0,"name":"Node Server","host":"localhost","port":3000},
-{"id":2,"clusterID":0,"name":"USERAdmin","host":"http://10.206.0.80","port":8091},
-{"id":3,"clusterID":0,"name":"Google","host":"google.com"}
-                ];
+var servers = [];
 
+servers.push({"id":0,"clusterID":0,"name":"OAS Test","host":"http://10.206.0.80","port":8090});
+servers.push({"id":1,"clusterID":0,"name":"OAS Admin","host":"http://10.206.0.80","port":8091})
+servers.push({"id":2,"clusterID":0,"name":"API Server","host":"https://10.206.0.81","port":3000});
+
+servers.push({"id":0,"clusterID":1,"name":"OAS Test","host":"http://10.206.0.80","port":4200});
+servers.push({"id":1,"clusterID":1,"name":"OAS Admin","host":"http://10.206.0.80","port":4201})
+servers.push({"id":2,"clusterID":1,"name":"API Server","host":"http://10.206.0.80","port":2000});
+
+servers.push({"id":3,"clusterID":2,"name":"pgAdmin","host":"http://10.206.0.80","port":5050});
+servers.push({"id":3,"clusterID":2,"name":"Google","host":"google.com"});
 exports.clusters = clusters;
 exports.servers = servers;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,31 +21,44 @@ export class AppComponent implements OnInit {
   constructor(private appService: AppService) {}
 
   ngOnInit() {
-    this.appService.getListOfClusters().then(response => {this.clusters = response.body; });
-    this.appService.getListOfServers().then(response => {this.servers = response.body;
+    this.appService.getListOfClusters().then(response => {
+      this.clusters = response.body;
+    });
+    this.appService.getListOfServers().then(response => {
+      this.servers = response.body;
+      this.getAllServerStats(this.servers);
+    });
+    const self = this;
+    setInterval(function() {
+      self.getAllServerStats(self.servers); }, 5000);
+  }
+  getAllServerStats(serverList) {
     this.serverIDArray = [];
-      if (this.servers && this.clusters) {
-        this.sortedServers = this.clusters;
-        for (const server of this.servers) {
-          for (const sortedServer of this.sortedServers) {
-            if (server.clusterID === sortedServer.clusterID) {
-              if (!sortedServer.servers) {
-                sortedServer['servers'] = [];
-              }
-              sortedServer.servers.push(server);
-              this.serverIDArray.push({'id' : server.id, 'status' : false});
-              break;
+    for(const sortedServer of this.sortedServers) {
+      sortedServer['servers'] = [];
+    }
+    if (this.servers && this.clusters) {
+      this.sortedServers = this.clusters;
+      for (const server of this.servers) {
+        for (const sortedServer of this.sortedServers) {
+          if (server.clusterID === sortedServer.clusterID) {
+            if (!sortedServer.servers) {
+              sortedServer['servers'] = [];
             }
+            sortedServer.servers.push(server);
+            this.serverIDArray.push({ id: server.id, status: false });
+            break;
           }
         }
-        this.interval1Cleared = true;
       }
-    const serverArray  = [];
+      this.interval1Cleared = true;
+    }
+    const serverArray = [];
     for (const serverID of this.serverIDArray) {
       serverArray.push(serverID.id);
     }
-    this.appService.getServerListStatus(serverArray)
-    .then(response1 => {this.serverIDArray = response1.body;
+    this.appService.getServerListStatus(serverArray).then(response1 => {
+      this.serverIDArray = response1.body;
       this.interval2Cleared = true;
       for (const cluster of this.sortedServers) {
         if (cluster.servers) {
@@ -59,7 +72,6 @@ export class AppComponent implements OnInit {
         }
       }
     });
-  });
 
   }
 }
