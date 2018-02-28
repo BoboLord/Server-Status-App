d88bd71f6641ec7368d45acbc0e575487a0a1d76
BoboLord_ <saisreedeep@outlook.com>
2018-03-01 00:21:32 +0530

    Fixed batch script
    Fixed npm issue using call npm

--- push-master.bat
+++ push-master.bat
@@ -3,11 +3,15 @@ git branch --set-upstream-to origin/development
 git pull
 git branch --set-upstream-to origin/master
 git push
-ng build --prod
-RMDIR "z:\hosting" /s /q
-MKDIR "z:\hosting"
+call npm run build
+
+timeout 5 >nul
+REM RMDIR "z:\hosting" /s /q
+REM MKDIR "z:\hosting"
+DEL /S /Q "z:\hosting"
+DEL /S /Q "z:\hosting\assets\"
 xcopy dist "z:\hosting\"
-xcopy dist\assets "z:\hosting\assets\" /i
+xcopy dist\assets "z:\hosting\assets\"
 git checkout development
 z:
 cd development/server-status-app
