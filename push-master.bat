872e11ee7e4d4ae78acdf3e513e295409ec3f840
BoboLord_ <saisreedeep@outlook.com>
2018-02-28 23:46:40 +0530

    Added a batch script to automate build and push master to cloud

--- push-master.bat
+++ push-master.bat
@@ -3,5 +3,13 @@ git branch --set-upstream-to origin/development
 git pull
 git branch --set-upstream-to origin/master
 git push
-
+ng build --prod
+RMDIR "z:\hosting" /s /q
+MKDIR "z:\hosting"
+xcopy dist "z:\hosting\"
+xcopy dist\assets "z:\hosting\assets\" /i
+git checkout development
+z:
+cd development/server-status-app
+git pull
 cmd /k
