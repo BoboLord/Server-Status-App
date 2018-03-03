eac3c7a5af794ad7a8fbd7ab2f56c7cbff503087
BoboLord_ <saisreedeep@outlook.com>
2018-03-03 17:50:03 +0530

    Removed development servers from default list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { AppService } from './services/app.service';
-
-import { AppComponent } from './app.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { StoredServerStatusComponent } from './components/server-status-app/server-status.component';
-import { PingToolComponent } from './components/ping-tool/ping-tool.component';
 import { HttpModule } from '@angular/http/src/http_module';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-
 import { RouterModule } from '@angular/router';
 import { InlineSVGModule } from 'ng-inline-svg';
 
+import { AppService } from './services/app.service';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { StoredServerStatusComponent } from './components/server-status-app/server-status.component';
+import { PingToolComponent } from './components/ping-tool/ping-tool.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+
 import { appRoutes } from './utilities/app-routing';
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, StoredServerStatusComponent, PingToolComponent, PageNotFoundComponent],
+  declarations: [AppComponent, NavbarComponent, StoredServerStatusComponent, PingToolComponent, LoginComponent,
+    RegistrationComponent, PageNotFoundComponent],
   imports: [BrowserModule, HttpClientModule, InlineSVGModule, FormsModule,
     RouterModule.forRoot(
       appRoutes,
@@ -29,4 +33,4 @@ import { appRoutes } from './utilities/app-routing';
   providers: [HttpClient, AppService],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/utilities/app-routing.ts
+++ src/app/utilities/app-routing.ts
@@ -3,9 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { StoredServerStatusComponent } from '../components/server-status-app/server-status.component';
 import { PingToolComponent } from '../components/ping-tool/ping-tool.component';
 import { PageNotFoundComponent } from '../components/page-not-found/page-not-found.component';
+import { LoginComponent } from '../components/login/login.component';
+import { RegistrationComponent } from '../components/registration/registration.component';
 
 export const appRoutes: Routes = [
   { path: '', component: StoredServerStatusComponent },
   { path: 'ping-tool', component: PingToolComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegistrationComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
