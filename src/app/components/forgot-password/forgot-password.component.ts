a1fe9b8cb83cf34fc3a4b38db5e4fe210a721136
BoboLord_ <saisreedeep@outlook.com>
2018-03-03 23:44:21 +0530

    Added forgot password page. Changed primary button CSS

---
+++ src/app/components/forgot-password/forgot-password.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { AppService } from '../../services/app.service';
+
+@Component({
+  selector: 'app-forgot-password',
+  templateUrl: './forgot-password.component.html',
+  styleUrls: ['./forgot-password.component.scss']
+})
+export class ForgotPasswordComponent implements OnInit {
+  constructor(private appService: AppService, private router: Router) { }
+
+  ngOnInit() { }
+}
