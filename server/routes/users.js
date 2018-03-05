c288ea8b4290aa5218a3256b6a43e9ecdc9b914b
BoboLord_ <saisreedeep@outlook.com>
2018-03-06 00:49:04 +0530

    Midwork push login css

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = new express.Router();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
 
 router.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
@@ -8,15 +12,33 @@ router.use(function (req, res, next) {
   next();
 });
 
-router.get('/login', (req, res) => {
-  res.send('login attempt');
-  router.post('/login', (req, res) => {
-    console.log(req.body.email)
-    res.json({
-      'XSRF-TOKEN': req
-    });
-  })
-});
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const hash = 'password'
+  bcrypt.compare(myPlaintextPassword, hash, function (err, response) {
+    if (err) {
+      console.log(err);
+      return err;
+    }
+    if (response === true) {
+      res.status(200).json({
+        message: 'Authentication was successful'
+      });
+    } else {
+      res.status(403).json({
+        message: 'Invalid username or password'
+      });
+    }
+  });
+})
+
+router.post('/registration', (req, res) => {
+  const email = req.body.email;
+  bcrypt.hash(myPlaintextPassword, saltRounds, function (err, hash) {
+
+  });
+
+})
 
 
 module.exports = router;
