8361199ee0f7b7b8ee2bd4b24ec7011fcaaba2f3
BoboLord_ <saisreedeep@outlook.com>
2018-03-31 00:18:07 +0530

    Changed baseURL for master

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -2,5 +2,5 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ConfigService {
     csrfToken: string;
-    baseURL = 'http://127.0.0.1:3000';
+    baseURL = 'https://api.obsidianserver.cf';
 }
