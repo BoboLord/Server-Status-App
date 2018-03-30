d7fd800c471dd70ee40c975af5df8951d987dcc5
BoboLord_ <saisreedeep@outlook.com>
2018-03-31 00:16:12 +0530

    Added configuration service

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { AppService } from '../../services/app.service';
-
+import { ConfigService } from './../../services/config.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  constructor(public appService: AppService, private router: Router) { }
+  constructor(private configService: ConfigService, public appService: AppService, private router: Router) { }
 
   ngOnInit() { }
 }
